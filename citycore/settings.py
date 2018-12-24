b25272e5392927bd0f83a663886ad3888f2d7fb3
mahmoudmarayef <mahmoudmarayef@gmail.com>
2018-12-24 19:37:51 +0200

    fourth commit

--- citycore/settings.py
+++ citycore/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'city_core/media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/static/',
+    os.path.join(BASE_DIR, "static")
 ]
