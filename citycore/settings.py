c62acacd25e2e541b7c30299d1e95470b0c45ce2
mahmoudmarayef <mahmoudmarayef@gmail.com>
2018-12-26 22:24:28 +0200

    deploy_to_herku settings

--- citycore/settings.py
+++ citycore/settings.py
@@ -119,15 +119,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'city_core/media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
-django_heroku.settings(locals())
