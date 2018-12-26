5838743e8160b5789ce4abfbb0ea5051257a132a
mahmoudmarayef <mahmoudmarayef@gmail.com>
2018-12-26 03:29:54 +0200

    dploy

--- citycore/settings.py
+++ citycore/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'city_core/media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+django_heroku.settings(locals())
--- citycore/urls.py
+++ citycore/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hello.views import home
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +23,5 @@ from hello.views import projects
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('projects/', projects, name='projects')
+    path('projects/', projects, name='projects'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
